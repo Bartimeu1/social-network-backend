611414ec43d04aca7e1e6c1d5f4ecfdfbc9c01a4
Ivan Sapego <sapego-ivan@list.ru>
2023-09-10 22:17:29 +0300

    feat: webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+import NodemonPlugin from 'nodemon-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: 'development',
+  target: 'node',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.cjs',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new NodemonPlugin()],
+};
